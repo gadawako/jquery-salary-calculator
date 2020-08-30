5fe2ace73cda7b09c5e637b331c32daf1a481693
Gada Wako <gada.wakooo@gmail.com>
2020-08-29 23:32:02 -0500

    append to dom

--- scripts/client.js
+++ scripts/client.js
@@ -1,20 +1,30 @@
+$(document).ready(onReady);
 
-$(document).ready(onReady)
+let monthlyCost = [];
 
-let montlyCost = []
-
-function onReady(){
-    $( '#submitButton' ).on( 'click' , submitBtn )
+function onReady() {
+  $("#submitButton").on("click", submitBtn);
+  displaySalary();
 }
-
-    function submitBtn(){
-        const newEmployee = {
-            first: $( '#firstName').val(),
-            last: $( '#lastName').val(),
-            id: $( '#idHolder').val(),
-            title: $( '#titleHolder').val(),
-            salary: $( '#annualSalary').val()
-        }
-        montlyCost.push(newEmployee);
-        console.log(newEmployee)
-    }
+function submitBtn() {
+  const newEmployee = {
+    first: $("#firstName").val(),
+    last: $("#lastName").val(),
+    id: $("#idHolder").val(),
+    title: $("#titleHolder").val(),
+    salary: $("#annualSalary").val(),
+  };
+  monthlyCost.push(newEmployee);
+  displaySalary();
+}
+function displaySalary() {
+  console.log("in displaySalary");
+  let el = $("#showSalary");
+  el.empty();
+  for (let i = 0; i < monthlyCost.length; i++) {
+    console.log(monthlyCost[i]);
+    el.append(`<li>${monthlyCost[i].first}
+             ${monthlyCost[i].last} ${monthlyCost[i].id} 
+             ${monthlyCost[i].title}  ${monthlyCost[i].salary}</li>`);
+  }
+}
